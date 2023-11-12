71769cd0c24126a01986163c75a00e1453f3706b
BillyChan0102 <135054136+BillyChan0102@users.noreply.github.com>
2023-11-12 22:12:55 +0800

    Add files via upload

---
+++ emotion.py
@@ -0,0 +1,28 @@
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+def get_emotion(sentence):
+    # Initialize the sentiment analyzer
+    sid = SentimentIntensityAnalyzer()
+
+    # Get the sentiment scores for the sentence
+    sentiment_scores = sid.polarity_scores(sentence)
+
+    # Determine the emotion based on the sentiment scores
+    if sentiment_scores['compound'] >= 0.05:
+        emotion = 'Positive'
+    elif sentiment_scores['compound'] <= -0.05:
+        emotion = 'Negative'
+    else:
+        emotion = 'Neutral'
+
+    # Calculate the emotion score
+    emotion_score = (sentiment_scores['compound'] + 1) * 50
+
+    return emotion, emotion_score
+
+# Test the function
+sentence = "I'm absolutely thrilled and overjoyed with the amazing news!"
+emotion, emotion_score = get_emotion(sentence)
+print("Emotion:", emotion)
+print("Emotion Score:", emotion_score)
